d23dc6f40cb574236ed9e40562b38291cea3de98
Brian Jackson <brianjackson1224@gmail.com>
2024-09-22 19:06:24 -0700

    Added logo and improved ui

--- ui/src/pages/Layout.js
+++ ui/src/pages/Layout.js
@@ -1,16 +1,21 @@
 import { Outlet, Link } from "react-router-dom";
+import logo from "../assets/logo.png"
 import '../styles/Layout.css';
 
 const Layout = () => {
     return(
         <>
-            <nav class="nav-container">
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
+            <nav class="top-container">
+                <img class="logo" src={logo} alt="Empty Music"/>
+                <ul class="nav-list">
+                    <li class="nav-item">
+                        <Link class="link" to="/"><p class="nav-text">Home</p></Link>
                     </li>
-                    <li>
-                        <Link to="/contact">Contact</Link>
+                    <li class="nav-item">
+                        <Link class="link" to="/artist/1"><p class="nav-text">My Music</p></Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link class="link" to="/contact"><p class="nav-text">Contact</p></Link>
                     </li>
                 </ul>
             </nav>
