5174ffb5882ea4d4d1b0b7ebcc11d22e9c8f3d46
Brian Jackson <brianjackson1224@gmail.com>
2024-10-07 14:53:50 -0700

    Clean up 2.

--- TheEmptyApp/Options/AzureOptions.cs
+++ TheEmptyApp/Options/AzureOptions.cs
@@ -1,13 +1,6 @@
 ﻿namespace TheEmptyApp.Options;
 
 public class AzureOptions {
-    /*
-     * "BlobUrl": "https://emptyappstorageaccount.blob.core.windows.net/emptyappcontainer1",
-     * "ResourceGroup": "BrianResourceGroup",
-     * "Account": "emptyappstorageaccount",
-     * "Container": "emptyappcontainer1"
-     * "ConnectionString": "DefaultEndpointsProtocol=https;AccountName=emptyappstorageaccount;AccountKey=H0CFVKZuIGxlnFFO54fMh6p9787qknEZqVapqNtBawPfrYZeSes0baXar4lb+udroNepPkRJ+nLa+AStzrYzzA==;EndpointSuffix=core.windows.net"
-     */
     public string? BlobUrl { get; set; }
     public string? ResourceGroup { get; set; }
     public string? Account { get; set; }
