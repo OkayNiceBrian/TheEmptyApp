ef7c0bd7dea537d9f657a647c3c9b28783a7ac5c
Brian Jackson <brianjackson1224@gmail.com>
2024-10-09 17:30:06 -0700

    separated album and artist pages

--- ui/src/pages/Album/Album.js
+++ ui/src/pages/Album/Album.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { PlayCircle02Icon, Edit02Icon, Delete04Icon, Queue02Icon } from "hugeicons-react";
 import { useAuth } from "contexts/AuthContext";
 import { useAudio } from "contexts/AudioPlayerContext";
@@ -7,12 +7,14 @@ import { apiHost, blobUrl } from "config/host";
 import "styles/Album.css";
 
 const Album = () => {
-    const { albumId } = useParams();
+    const { artistId, albumId } = useParams();
     const { token, userArtistId } = useAuth();
     const { playSong, queueSong } = useAudio();
 
     const [album, setAlbum] = useState();
     const [loading, setLoading] = useState(true);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (loading) {
@@ -32,22 +34,32 @@ const Album = () => {
         }
     })
 
-    const onClickDeleteAlbum = () => {
-
+    const onClickDeleteAlbum = (albumId) => {
+        const url = apiHost + "/albums/" + albumId;
+        fetch(url, {
+            method: "DELETE",
+            headers: {
+                "Authorization": "Bearer " + token
+            }
+        }).then(rsp => {
+            if (rsp.status === 204) {
+                navigate(`/artist/${artistId}`)
+            };
+        }).catch(e => console.error(e));
     }
 
     const renderAlbum = () => {
         return (
             <div className="album-container">
-                <div className="album-header-container">
+                <div className="album-cover-container">
                     <img className="album-cover" src={blobUrl + "/" + album.coverImageGuid} alt={album.name}/>
                     <PlayCircle02Icon className={"clickable-icon"} color={"cornflowerblue"} size={"100px"}/>
                     <div className="album-header-items-container">
-                        {userArtistId === album.artistId && <Edit02Icon className="clickable-icon" color={"green"} onClick={() => onClickDeleteAlbum(album.id)}/>}
-                        {userArtistId === album.artistId && <Delete04Icon className="clickable-icon" color={"red"} onClick={() => onClickDeleteAlbum(album.id)}/>}
+                        {userArtistId === artistId && <Edit02Icon className="clickable-icon" color={"green"} onClick={() => onClickDeleteAlbum(album.id)}/>}
+                        {userArtistId === artistId && <Delete04Icon className="clickable-icon" color={"red"} onClick={() => onClickDeleteAlbum(album.id)}/>}
                     </div>
                 </div> 
-                <ul className="songs-container">
+                <ul className="album-songs-container">
                     {renderSongs(album)}
                 </ul>
             </div>
@@ -56,21 +68,21 @@ const Album = () => {
 
     const renderSongs = (album) => {
         return album.songs.map((song, index) => 
-            <li key={song.id} className="song-container-grid" style={{borderTopWidth: index === 0 ? "1px" : 0}}>
-                <p className="song-text">{song.trackNum}</p>
+            <li key={song.id} className="album-song-container-grid" style={{borderTopWidth: index === 0 ? "1px" : 0}}>
+                <p className="album-song-text">{song.trackNum}</p>
                 <PlayCircle02Icon className={"clickable-icon"} color={"cornflowerblue"} onClick={() => playSong({artistName: "somthin", albumName: album.name, songName: song.name, guid: song.audioFileGuid, coverImageGuid: album.coverImageGuid})}/>
-                <p className="song-title-text">{song.name}</p>
-                <p className="song-text">{album.name}</p>
-                <p className="song-text">{/*artist.name*/}</p>
-                <p className="song-text">3:23</p>
+                <p className="album-song-title-text">{song.name}</p>
+                <p className="album-song-text">{album.name}</p>
+                <p className="album-song-text">{/*artist.name*/}</p>
+                <p className="album-song-text">3:23</p>
                 <Queue02Icon className={"clickable-icon"} color={"green"} size={"20px"} onClick={() => queueSong({artistName: "somethin", albumName: album.name, songName: song.name, guid: song.audioFileGuid, coverImageGuid: album.coverImageGuid})}/>
             </li>
         );
     }
 
     if (loading) return (
-        <div className="album-container">
-            <p className="header-text">loading...</p>
+        <div className="album-body">
+            <p className="album-header-text">loading...</p>
         </div>
     );
 
--- ui/src/pages/Artist/Artist.js
+++ ui/src/pages/Artist/Artist.js
@@ -1,16 +1,14 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { Delete04Icon, Edit02Icon, AddCircleIcon, PlayCircle02Icon, Queue02Icon } from "hugeicons-react";
+import { Delete04Icon, AddCircleIcon} from "hugeicons-react";
 import { useAuth } from "contexts/AuthContext";
-import { useAudio } from "contexts/AudioPlayerContext";
 import { apiHost, blobUrl } from "config/host";
 import "styles/Artist.css";
 
 const Artist = () => {
     const { artistId } = useParams();
 
-    const { token } = useAuth();
-    const { queueSong, playSong } = useAudio();
+    const { token, userArtistId } = useAuth();
 
     const [isLoading, setIsLoading] = useState(true);
     const [artist, setArtist] = useState(null);
@@ -54,35 +52,10 @@ const Artist = () => {
 
     const renderAlbums = () => {
         return artist.albums.map(album => 
-            <div key={album.id} className="album-container">
-                <div className="album-header-container">
-                    <img className="album-cover" src={blobUrl + "/" + album.coverImageGuid} alt={album.name}/>
-                    {//<p className="album-header-text">{album.name}</p>
-                    }
-                    <PlayCircle02Icon onClick={() => onClickAlbum(album.id)} className={"clickable-icon"} color={"cornflowerblue"} size={"100px"}/>
-                    <div className="album-header-items-container">
-                        <Edit02Icon className="clickable-icon" color={"green"} onClick={() => onClickDeleteAlbum(album.id)}/>
-                        <Delete04Icon className="clickable-icon" color={"red"} onClick={() => onClickDeleteAlbum(album.id)}/>
-                    </div>
-                </div> 
-                <ul className="songs-container">
-                    {renderSongs(album)}
-                </ul>
+            <div onClick={() => onClickAlbum(album.id)} key={album.id} className="artist-album-container">
+                <img className="artist-album-cover" src={blobUrl + "/" + album.coverImageGuid} alt={album.name}/>
+                <p className="artist-album-header-text">{album.name}</p>
             </div>
-        );
-    }
-
-    const renderSongs = (album) => {
-        return album.songs.map((song, index) => 
-            <li key={song.id} className="song-container-grid" style={{borderTopWidth: index === 0 ? "1px" : 0}}>
-                <p className="song-text">{song.trackNum}</p>
-                <PlayCircle02Icon className={"clickable-icon"} color={"cornflowerblue"} onClick={() => playSong({artistName: artist.name, albumName: album.name, songName: song.name, guid: song.audioFileGuid, coverImageGuid: album.coverImageGuid})}/>
-                <p className="song-title-text">{song.name}</p>
-                <p className="song-text">{album.name}</p>
-                <p className="song-text">{artist.name}</p>
-                <p className="song-text">3:23</p>
-                <Queue02Icon className={"clickable-icon"} color={"green"} size={"20px"} onClick={() => queueSong({artistName: artist.name, albumName: album.name, songName: song.name, guid: song.audioFileGuid, coverImageGuid: album.coverImageGuid})}/>
-            </li>
         );
     }
 
@@ -102,29 +75,9 @@ const Artist = () => {
         }
     }
 
-    const onClickDeleteAlbum = async (albumId) => {
-        try {
-            const url = apiHost + "/albums/" + albumId;
-            await fetch(url, {
-                method: "DELETE",
-                headers: {
-                    "Authorization": "Bearer " + token
-                }
-            }).then(rsp => {
-                if (rsp.status === 204) {
-                    artist.albums = artist.albums.filter(album => album.id !== albumId);
-                    setArtist(artist);
-                    setIsLoading(true);
-                };
-            })
-        } catch (e) {
-            console.error(e);
-        }
-    }
-
     if (isLoading) return (
-        <div className="container">
-            <p className="header-text">loading...</p>
+        <div className="artist-container">
+            <p className="artist-header-text">loading...</p>
         </div>
     );
 
@@ -132,10 +85,13 @@ const Artist = () => {
         <div className="artist-container">
             <div className="artist-header-container">
                 <p className="artist-header-text">{artist.name}</p>
-                <Delete04Icon className="clickable-icon" color={"red"} onClick={onClickDelete}/>
+                {//userArtistId === artistId && <Delete04Icon className="clickable-icon" color={"red"} onClick={onClickDelete}/>
+                }
             </div>
-            {renderAlbums()}
-            <AddCircleIcon className="clickable-icon" color={"green"} size={"40px"} onClick={() => navigate(`/artist/${artistId}/create/album`)} style={{marginBottom: "100px"}}/>
+            <div className="artist-album-list-container">
+                {renderAlbums()}
+            </div>
+            {userArtistId === artistId && <AddCircleIcon className="clickable-icon" color={"green"} size={"40px"} onClick={() => navigate(`/artist/${artistId}/album/create`)} style={{marginBottom: "100px"}}/>}
         </div>
     );
 };
