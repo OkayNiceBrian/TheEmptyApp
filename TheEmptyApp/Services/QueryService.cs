9c80cf48fbc6554cec73a0ef6c2f5622e46bc8be
Brian Jackson <brianjackson1224@gmail.com>
2024-10-16 11:33:16 -0700

    Search algorithm done for now.

--- TheEmptyApp/Services/QueryService.cs
+++ TheEmptyApp/Services/QueryService.cs
@@ -10,23 +10,32 @@ public class QueryService : IQueryService {
     public QueryService(ApplicationDbContext ctx) => _ctx = ctx;
 
     public async Task<List<Artist>> QueryArtist(string queryString, int pagesToSkip, int itemsPerPage) {
-        return await _ctx.Artists.Skip(pagesToSkip * itemsPerPage).Take(itemsPerPage)
+        return await _ctx.Artists
             .Include(a => a.Albums)
+            .Where(a => a.Name!.ToLower().Contains(queryString.ToLower())
+            || a.Albums.Select(a => a.Name!.ToLower()).Contains(queryString.ToLower()) 
+            || a.Albums.SelectMany(a => a.Songs.Select(s => s.Name!.ToLower())).ToList().Contains(queryString.ToLower()))
+            .Skip(pagesToSkip * itemsPerPage).Take(itemsPerPage)
             .ToListAsync();
     }
     public async Task<List<Album>> QueryAlbum(string queryString, int pagesToSkip, int itemsPerPage) {
-        return await _ctx.Albums.Skip(pagesToSkip * itemsPerPage).Take(itemsPerPage)
+        return await _ctx.Albums
             .Include(a => a.Artist)
             .Include(a => a.Songs)
-            .OrderByDescending(a => a.ReleaseDate)
+            .Where(a => a.Name!.ToLower().Contains(queryString.ToLower())
+            || a.Artist!.Name!.ToLower().Contains(queryString.ToLower())
+            || a.Songs.Select(s => s.Name!.ToLower()).Contains(queryString.ToLower()))
+            .Skip(pagesToSkip * itemsPerPage).Take(itemsPerPage)
             .ToListAsync();
     }
     public async Task<List<Song>> QuerySong(string queryString, int pagesToSkip, int itemsPerPage) {
-        return await _ctx.Songs.Where(s => s.Name!.ToLower().Contains(queryString.ToLower()))
-            .Skip(pagesToSkip * itemsPerPage).Take(itemsPerPage)
+        return await _ctx.Songs
             .Include(s => s.Artist)
             .Include(s => s.Album)
-            .OrderByDescending(s => s.Album!.ReleaseDate)
+            .Where(s => s.Name!.ToLower().Contains(queryString.ToLower()) 
+            || s.Album!.Name!.ToLower().Contains(queryString.ToLower()) 
+            || s.Artist!.Name!.ToLower().Contains(queryString.ToLower()))
+            .Skip(pagesToSkip * itemsPerPage).Take(itemsPerPage)
             .ToListAsync();
     }
 }
