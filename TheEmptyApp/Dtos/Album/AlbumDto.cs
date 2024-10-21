b0110ee66e054ee1990f1a2b31e349bf65fb4a94
Brian Jackson <brianjackson1224@gmail.com>
2024-10-21 15:59:31 -0700

    Album Dtos updates

--- TheEmptyApp/Dtos/Album/AlbumDto.cs
+++ TheEmptyApp/Dtos/Album/AlbumDto.cs
@@ -12,5 +12,7 @@ public class AlbumDto {
     public DateOnly? ReleaseDate { get; set; }
     public string PrimaryGenre { get; set; } = string.Empty;
     public string SecondaryGenre { get; set; } = string.Empty;
+    public bool IsPrivate { get; set; } = false;
+    public List<string> AllowedEmails { get; set; } = [];
     public string CoverImageGuid { get; set; } = string.Empty;
 }
--- TheEmptyApp/Dtos/Album/UpdateAlbumDto.cs
+++ TheEmptyApp/Dtos/Album/UpdateAlbumDto.cs
@@ -6,5 +6,7 @@ public class UpdateAlbumDto {
     public DateOnly? ReleaseDate { get; set; }
     public string PrimaryGenre { get; set; } = string.Empty;
     public string SecondaryGenre { get; set; } = string.Empty;
+    public bool IsPrivate { get; set; } = false;
+    public List<string> AllowedEmails { get; set; } = [];
     public string CoverImageGuid { get; set; } = string.Empty;
 }
