643b3af1edfbddf06661401513ab9422d040a86d
Brian Jackson <brianjackson1224@gmail.com>
2024-11-20 13:28:13 -0800

    AudioPlayer made responsive

--- ui/src/components/AudioPlayer.jsx
+++ ui/src/components/AudioPlayer.jsx
@@ -1,11 +1,13 @@
 import { useState, memo } from 'react';
 import { Link } from 'react-router-dom';
 import { PauseIcon, PlayCircle02Icon, PreviousIcon, VolumeHighIcon } from "hugeicons-react";
+import { useViewport } from "contexts/ViewportContext";
 import { blobUrl } from "config/host";
 import { convertDuration } from 'helpers/Util';
 import "./styles/AudioPlayer.css";
 
 const AudioPlayer = memo(({ trackInfo, isVisible, isPaused, setIsPaused, isPlayerLoading, lastPlayedTrack, isPlaying, playSong, skipSong, queueLength, currentTime, volume, setVolume }) => {
+    const { width } = useViewport();
 
     const onClickPlay = () => {
         if (lastPlayedTrack && !isPlaying) {
@@ -17,11 +19,11 @@ const AudioPlayer = memo(({ trackInfo, isVisible, isPaused, setIsPaused, isPlaye
     return (
         <div style={{display: isVisible ? "flex" : "none"}} className="audioPlayer-container">
             <div className="audioPlayer-info-container">
-                <img className={"audioPlayer-cover-image"} src={blobUrl + "/" + trackInfo.coverImageGuid} alt={"cover"}/>
-                <div className={"audioPlayer-info-text-container"}>
+                <Link to={`/artist/${trackInfo.artistId}/album/${trackInfo.albumId}`}><img className={"audioPlayer-cover-image"} src={blobUrl + "/" + trackInfo.coverImageGuid} alt={"cover"}/></Link>
+                {width > 800 && <div className={"audioPlayer-info-text-container"}>
                     <Link className='audioPlayer-link' to={`/artist/${trackInfo.artistId}/album/${trackInfo.albumId}`}><p className="audioPlayer-info-text-song">{trackInfo.songName}</p></Link>
                     <Link className='audioPlayer-link' to={`/artist/${trackInfo.artistId}`}><p className="audioPlayer-info-text-artist">{trackInfo.artistName}</p></Link>
-                </div>
+                </div> }
             </div>
             <div className="audioPlayer-controls-container">
                 <PreviousIcon className={"audioPlayer-icon-inactive"} size={"30px"}/>
