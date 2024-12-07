dd01da3e6d4894391b6ecea4a8510b566bb9d944
Brian Jackson <brianjackson1224@gmail.com>
2024-12-07 12:39:13 -0800

    Messing around with mui contact page

--- ui/src/pages/Contact.jsx
+++ ui/src/pages/Contact.jsx
@@ -1,9 +1,22 @@
-import { Box, Container } from "@mui/material";
+import { Box, Button, Container, Paper, TextareaAutosize, TextField, Typography } from "@mui/material";
 
 const Contact = () => {
     return (
         <Container>
-            <Box></Box>
+            <Box display="flex" flexDirection="column" alignItems="center" pt="2rem" gap="2rem">
+                <Typography variant="h2">Contact</Typography>
+                <Paper elevation={16}>
+                    <Box p={"2rem"} display="flex" flexDirection={"column"}>
+                        <Typography>Email Address</Typography>
+                        <TextField variant="outlined" size="small"></TextField>
+                        
+                        <Typography sx={{mt: "2rem"}}>Message</Typography>
+                        <TextareaAutosize variant="outlined" size="large"></TextareaAutosize>
+
+                        <Button variant={"contained"} sx={{mt: "2rem"}}>Submit</Button>
+                    </Box>
+                </Paper>
+            </Box>
         </Container>
     );
 }
